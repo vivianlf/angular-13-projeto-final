05715dce77598c440d20d96e52b0ad31045699ed
Vivian <vivianleitef@gmail.com>
2022-03-20 16:08:17 -0300

    Projeto final

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
+import { CalculadoraModule } from './calculadora/calculadora.module';
+import { ConversorModule } from './conversor/conversor.module';
+import { DashboardModule } from './dashboard/dashboard.module';
+import { JogoDaVelhaModule } from './jogo-da-velha/jogo-da-velha.module';
+import { TarefasModule } from './tarefas/tarefas.module';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    DashboardModule,
+    CalculadoraModule,
+    ConversorModule,
+    TarefasModule,
+    JogoDaVelhaModule,
+    AppRoutingModule
+ 
   ],
   providers: [],
   bootstrap: [AppComponent]
